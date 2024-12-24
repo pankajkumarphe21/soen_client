e87d9257b91a348163a3519b50c7ec77e0b695ab
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-12-24 09:04:46 +0530

    first commit

---
+++ src/context/user.context.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// Create the UserContext
+export const UserContext = createContext();
+
+// Create a provider component
+export const UserProvider = ({ children }) => {
+    const [ user, setUser ] = useState(null);
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
