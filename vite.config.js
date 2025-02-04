1c343c0d8228b1bdd954b787240bc204e3b0b795
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2025-02-04 12:50:00 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,6 @@ export default defineConfig({
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin"
-    },
-    proxy: {
-      '/cdn': {
-        target: 'https://unpkg.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cdn/, '')
-      }
     }
   }
 })
